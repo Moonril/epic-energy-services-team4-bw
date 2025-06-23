e9701c6130e3498192c1e0c802f07e0faf43aea0
Vincenza <vinni2690@hotmail.com>
2025-06-23 11:38:29 +0200

    Initial commit

---
+++ src/main/java/it/epicode/epic_energy_services_team4_bw/EpicEnergyServicesTeam4BwApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.epic_energy_services_team4_bw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyServicesTeam4BwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyServicesTeam4BwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/epic_energy_services_team4_bw/EpicEnergyServicesTeam4BwApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.epic_energy_services_team4_bw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServicesTeam4BwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
