ccb469713184174e538b3287d899cf73e3291195
Vincenza <vinni2690@hotmail.com>
2025-06-23 15:04:19 +0200

    provincia e regione

--- src/main/java/it/epicode/epic_energy_services_team4_bw/model/Comune.java
+++ src/main/java/it/epicode/epic_energy_services_team4_bw/model/Comune.java
@@ -1,9 +1,27 @@
 package it.epicode.epic_energy_services_team4_bw.model;
 
-import jakarta.persistence.Entity;
+
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Entity
+@Table(name = "comuni")
 public class Comune {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String nome;
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "provincia_id")
+    private Provincia provincia;
+
+    @OneToMany(mappedBy = "comune")
+    private List<Indirizzo> indirizzi;
+
 }
--- src/main/java/it/epicode/epic_energy_services_team4_bw/model/Provincia.java
+++ src/main/java/it/epicode/epic_energy_services_team4_bw/model/Provincia.java
@@ -1,9 +1,24 @@
 package it.epicode.epic_energy_services_team4_bw.model;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Entity
+@Table(name = "province")
 public class Provincia {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String nome;
+    private String sigla;
+    private String regione;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "provincia")
+    private List<Comune> comuni;
+
 }
